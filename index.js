512d3d9107b1b5af1aa6009d404833a1ba5738c0
Leilani Migisi <leilani.migisi@student.moringaschool.com>
2022-06-06 15:31:35 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Leilani Migisi';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
